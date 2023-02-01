5e542065f9e623a588497390846db391e139648c
larissacarletti <larissalealcarletti@gmail.com>
2023-02-01 19:02:43 -0300

    initial commit

--- app/src/main/java/com/example/tasky/api/TaskyApi.kt
+++ app/src/main/java/com/example/tasky/api/TaskyApi.kt
@@ -3,6 +3,7 @@ package com.example.tasky.api
 import com.example.tasky.model.Task
 import retrofit2.Response
 import retrofit2.http.GET
+
 interface TaskyApi {
     @GET("/todos")
     suspend fun getTodos() : Response<List<Task>>
